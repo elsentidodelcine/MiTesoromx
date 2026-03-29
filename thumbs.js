651805f9f48d6cf385aa42ceeaa986befc008cf3
elsentidodelcine <elsentidodelcine@gmail.com>
2026-03-28 19:01:19 -0600

    Imagenes

--- thumbs.js
+++ thumbs.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
 
-const ORIGEN = path.join(__dirname, "MarzoFin");
+const ORIGEN = path.join(__dirname, "AbrilIni");
 const DESTINO = path.join(__dirname, "thumbs");
 
 // crear carpeta thumbs si no existe
