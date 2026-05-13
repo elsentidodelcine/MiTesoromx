dffb066a1de7667d6a331d27a29aa8de1455514c
elsentidodelcine <elsentidodelcine@gmail.com>
2026-05-13 12:13:00 -0600

    nuevos productos

--- thumbs.js
+++ thumbs.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
 
-const ORIGEN = path.join(__dirname, "Nuevas");
+const ORIGEN = path.join(__dirname, "Mayo");
 const DESTINO = path.join(__dirname, "thumbs");
 
 // crear carpeta thumbs si no existe
